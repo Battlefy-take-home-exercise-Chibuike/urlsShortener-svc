018f351c45f0df9bf3c086d7137a7f77bef53289
Chibuike <nnadi.chibuike@yahoo.com>
2023-02-26 11:07:59 -0500

    Initial commit

---
+++ test/url-shortener-svc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UrlShortenerSvc from '../lib/url-shortener-svc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/url-shortener-svc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UrlShortenerSvc.UrlShortenerSvcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
